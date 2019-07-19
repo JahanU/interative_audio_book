36207245fc16c43a67bc3a6ef2420f020614cbd5
Jahan <jahanulhaque@gmail.com>
2019-07-19 23:52:46 +0100

    Update story_page.dart

--- lib/story_page.dart
+++ lib/story_page.dart
@@ -6,18 +6,29 @@ class StoryPage extends StatelessWidget {
   final Story selectedStory;
   StoryPage(this.selectedStory);
 
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text(selectedStory.title),
+          centerTitle: true,
+          backgroundColor: Colors.teal,
         ),
         body: Center(
-          child: Text(selectedStory.title),
+          child: new Container(
+              decoration: new BoxDecoration(
+                  border: new Border.all(
+                      color: Colors.white
+                  )
+              ),
+              child: Image.network(
+                selectedStory.image,
+                width: 400.0,
+                height: 200.0,
+              )),
         ),
       ),
     );
